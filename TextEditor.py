cac9e3a6c3170cd36c836e90c7cf4298763e7164
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2024-01-31 12:46:26 -0500

    process unwanted control-{key} in TextEditor.py

--- TextEditor.py
+++ TextEditor.py
@@ -5,11 +5,8 @@ import re
 import TextEditorStack as tes
 
 """
-features to implement, redo, need to handle other special keys, like control + others
-set them to doing nothing instead of inputting blank boxes
-And maybe fix undo cursor index after undoing?
-refactor and create a function for fixing cursor after editions?
-Does not allow group deletion yet!
+Syntax highlighting?
+Indentation?
 """
 
 class TextEditor:
@@ -33,6 +30,13 @@ class TextEditor:
         self.text_space.bind('<Control-f>', lambda event: self.search_text())
         self.text_space.bind('<Control-h>', lambda event: self.clear_highlights())
         self.text_space.bind('<Control-q>', lambda event: self.root.destroy())
+
+        # bind unused keys to do nothing to prevent inputting garbage 
+        unused_keys = ['a', 'b', 'c', 'd', 'e', 'g', 'i', 'j', 'k'
+                    , 'l', 'm', 'n', 'p', 'r', 't', 'v', 'w', 'x', 'y', 'z']
+        for key in unused_keys:
+            self.text_space.bind(f'<Control-{key}>', self.do_nothing)
+        
 
     def setup_menu(self):
         menu_bar = tk.Menu(self.root)
@@ -105,14 +109,18 @@ class TextEditor:
     def on_backspace_key(self, event):
         """add feature to delete single char or a group of text"""
         cursor_index = self.text_space.index(tk.INSERT)
+        # delete one char when not selecting a group of text and not at the beginning 
         if cursor_index != "1.0" and not self.text_space.tag_ranges(tk.SEL):
             current_text = self.text_space.get("1.0", tk.END)
             self.undo_stack.push(current_text)
+
             plain_index = self.convert_to_plain_index(cursor_index)
             self.text_editor.delete(plain_index - 1, 1)
+
             updated_text = self.text_editor.form_text()
             self.text_space.delete("1.0", tk.END)
             self.text_space.insert("1.0", updated_text)
+
             new_cursor_index = self.convert_to_tk_index(plain_index - 1)
             self.text_space.mark_set(tk.INSERT, new_cursor_index)
         elif self.text_space.tag_ranges(tk.SEL):
@@ -124,12 +132,16 @@ class TextEditor:
             plain_start = self.convert_to_plain_index(start_index)
             plain_end = self.convert_to_plain_index(end_index)
             length_to_delete = plain_end - plain_start
+
             self.text_editor.delete(plain_start, length_to_delete)
             updated_text = self.text_editor.form_text()
             self.text_space.delete("1.0", tk.END)
             self.text_space.insert("1.0", updated_text)
+            # cursor is the at the same position as the start
+            # because we are deleting a block of text
             new_cursor_index = self.convert_to_tk_index(plain_start)
             self.text_space.mark_set(tk.INSERT, new_cursor_index)
+        # prevent default backspace behavior
         return "break"
 
     def on_delete_key(self, event):
@@ -234,6 +246,10 @@ class TextEditor:
 
     def clear_highlights(self):
         self.text_space.tag_remove("found", "1.0", tk.END)
+    
+    def do_nothing(self, event):
+        return "break"
+
 
 # Main application setup
 root = tk.Tk()
